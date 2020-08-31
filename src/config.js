92552032c7862c74eacdef4d66fbfc994020a6f5
Lauren <laurenwillia18@gmail.com>
2020-08-31 12:53:50 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-1",
+      BUCKET: "laurens-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://3c1knxd5p0.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_oPgbxetkj",
+      APP_CLIENT_ID: "2eblbtn6ja8ltg1qi2307o7ik9",
+      IDENTITY_POOL_ID: "us-west-2:28a637df-5b6b-4d16-96ce-236bcc0cf549"
+    }
+  };
