3b780099ed4e37c77d97c889f58e3df115c8786c
Lauren <laurenwillia18@gmail.com>
2020-09-02 16:14:47 -0600

    Adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,43 @@
-export function onError(error) {
-    let message = error.toString();
-  
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
-    }
-  
-    alert(message);
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
   }
+
+  Sentry.init({
+    dsn: "https://9ffbf13d4d1d4a5e8b006388117f277c@o442877.ingest.sentry.io/5415611"
+  });
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
+export function onError(error) {
+  let errorInfo = {};
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
+
+  logError(error, errorInfo);
+
+  alert(message);
+}
